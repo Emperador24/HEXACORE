714f42636b543a856b979cea89025261e24eeeb3
Juan Sebastian Sanchez Vasquez <tiansv@gmail.com>
2026-09-21 08:29:52 -0500

    Integra servicio de pedidos con Docker y API Gateway

--- App/services/pedidos/src/app.module.ts
+++ App/services/pedidos/src/app.module.ts
@@ -7,9 +7,11 @@ import { AutenticacionModule } from './comun/autenticacion/autenticacion.module'
 import { CatalogoModule } from './catalogo/catalogo.module';
 import { ConfigModule } from './config/config.module';
 import { PersistenciaModule } from './persistencia/persistencia.module';
+import { SaludController } from './salud/salud.controller';
 
 @Module({
   imports: [ConfigModule, PersistenciaModule, CatalogoModule, AutenticacionModule, CheckoutModule, PagosModule, InventarioModule],
   providers: [ExpiracionReservasService],
+  controllers: [SaludController],
 })
 export class AppModule {}
