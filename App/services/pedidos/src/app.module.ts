00efd7ec669591388b3740809b968a579adce13d
Juan Sebastian Sanchez Vasquez <tiansv@gmail.com>
2026-09-21 07:21:00 -0500

    Expira checkouts y libera inventario reservado

--- App/services/pedidos/src/app.module.ts
+++ App/services/pedidos/src/app.module.ts
@@ -1,4 +1,6 @@
 import { PagosModule } from './pagos/pagos.module';
+import { InventarioModule } from './inventario/inventario.module';
+import { ExpiracionReservasService } from './inventario/expiracion-reservas.service';
 import { Module } from '@nestjs/common';
 import { CheckoutModule } from './checkout/checkout.module';
 import { AutenticacionModule } from './comun/autenticacion/autenticacion.module';
@@ -7,6 +9,7 @@ import { ConfigModule } from './config/config.module';
 import { PersistenciaModule } from './persistencia/persistencia.module';
 
 @Module({
-  imports: [ConfigModule, PersistenciaModule, CatalogoModule, AutenticacionModule, CheckoutModule, PagosModule],
+  imports: [ConfigModule, PersistenciaModule, CatalogoModule, AutenticacionModule, CheckoutModule, PagosModule, InventarioModule],
+  providers: [ExpiracionReservasService],
 })
 export class AppModule {}
